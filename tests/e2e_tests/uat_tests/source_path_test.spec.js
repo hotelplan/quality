5db025a3e38632b24c94e1df28f4b718e9b64cde
xtcar7 <xtian.tcar@gmail.com>
2024-07-17 17:20:53 +0800

    added check to not redirect on error-500

--- tests/e2e_tests/uat_tests/source_path_test.spec.js
+++ tests/e2e_tests/uat_tests/source_path_test.spec.js
@@ -4,9 +4,9 @@ import {parse} from 'csv-parse/sync';
 import fs from 'fs';
 import path from 'path';
 
-test.afterEach(async ({ page },testInfo) => {
-  await page.close();
-});
+//test.afterAll(async ({ page },testInfo) => {
+  //await page.close();
+//});
 
 // Helper function to read URLs from the CSV file
 const SkiDatacsv = parse(fs.readFileSync(path.join(__dirname, 'uat_data', 'Migration_Ski.csv')), {
@@ -22,12 +22,13 @@ for(const skiData of SkiDatacsv){
 
     const homePath = 'https://inghamsv2-ecms.stg.hotelplan.co.uk';
     const sourcePath = skiData.SourcePath.replace("home", "");
-    //const sourceURL = sourcePath.replace(/\\/g, '/');
-    const FullURL = homePath + sourcePath;
+    const sourceURL = sourcePath.replace("ski-resorts","resorts");
+    const FullURL = homePath + sourceURL;
     // Open the URL
     const response = await page.goto(FullURL, { waitUntil: 'domcontentloaded' });
     console.log('TEST = '+ FullURL);
     console.log('Response = '+ response.status());
+    await expect(page).not.toHaveURL("https://inghamsv2-ecms.stg.hotelplan.co.uk/error-500")
     expect(response.status()).not.toBe(404);
     expect(response.status()).not.toBe(500);
   });
