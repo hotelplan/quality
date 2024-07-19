07afcbe9537fd5c3e76afdd595d9d09d5903ef20
xtcar7 <xtian.tcar@gmail.com>
2024-07-20 02:06:48 +0800

    added prod test

--- tests/e2e_tests/uat_tests/prod_source_path_test.spec.js
+++ tests/e2e_tests/uat_tests/prod_source_path_test.spec.js
@@ -11,10 +11,8 @@ import path from 'path';
 
 
 //path of the UAT site
-//const HOMEpath = 'https://inghamsv2-ecms.stg.hotelplan.co.uk';
-const HOMEpath = 'https://inghams-v2.newdev.hotelplan.co.uk';
-//const ERRORpath ='https://inghamsv2-ecms.stg.hotelplan.co.uk/error-500';
-const ERRORpath = 'https://inghams-v2.newdev.hotelplan.co.uk/error-500';
+const HOMEpath = 'https://www.inghams.co.uk';
+const ERRORpath = 'https://www.inghams.co.uk/error-500';
 
 // Helper function to read URLs from the CSV file
 const LaplandDatacsv = parse(fs.readFileSync(path.join(__dirname, 'uat_data', 'Migration_Lapland.csv')), {columns: true, skip_empty_lines: true});
@@ -26,7 +24,7 @@ const WalkingDatacsv = parse(fs.readFileSync(path.join(__dirname, 'uat_data', 'M
 // Define the test suite
 test.describe('Lapland Source Path', () => {
 for(const laplandData of LaplandDatacsv){
-  test(`Lapland (${laplandData.Country}) test: ${laplandData.SourcePath}`, async ({ page }) => {
+  test(`Lapland source path test for ${laplandData.SourcePath}`, async ({ page }) => {
     test.setTimeout(3000000);
 
     const sourcePath = laplandData.SourcePath.replace("home", "");
@@ -46,10 +44,10 @@ for(const laplandData of LaplandDatacsv){
 
 
 for(const santasData of SantaDatacsv){
-  test(`Santa Breaks (${santasData.Country}) test: ${santasData.SourcePath}`, async ({ page }) => {
+  test(`Santas source path test for ${santasData.SourcePath}`, async ({ page }) => {
     test.setTimeout(3000000);
 
-    const sourcePath = laplandData.SourcePath.replace("home", "");
+    const sourcePath = santasData.SourcePath.replace("home", "");
     const sourceURL = sourcePath.replace(/lapland-/g,"lapland");
     const sourceURLmod = sourceURL.replace("laplandholidays","lapland-holidays");
     const FullURL = HOMEpath + sourceURLmod;
@@ -68,12 +66,11 @@ for(const santasData of SantaDatacsv){
 
 test.describe('Ski Source Path', () => {
 for(const skiData of SkiDatacsv){
-  test(`Ski (${skiData.Country}) test: ${skiData.SourcePath}`, async ({ page }) => {
+  test(`Ski source path test for ${skiData.SourcePath}`, async ({ page }) => {
     test.setTimeout(3000000);
 
     const sourcePath = skiData.SourcePath.replace("home", "");
-    const sourceURL = sourcePath.replace("ski-resorts","resorts");
-    const FullURL = HOMEpath + sourceURL;
+    const FullURL = HOMEpath + sourcePath;
     // Open the URL
     const response = await page.goto(FullURL, { waitUntil: 'domcontentloaded' });
     console.log('TEST = '+ FullURL);
@@ -89,10 +86,11 @@ for(const skiData of SkiDatacsv){
 
 test.describe('Walking Source Path', () => {
 for(const walkingData of WalkingDatacsv){
-  test(`Walking (${walkingData.Country}) test: ${walkingData.SourcePath}`, async ({ page }) => {
+  test(`Walking source path test for ${walkingData.SourcePath}`, async ({ page }) => {
     test.setTimeout(3000000);
 
-    const sourcePath = walkingData.SourcePath.replace("home", "/walking-holidays");
+    const sourcePath = walkingData.SourcePath.replace("home", "");
+    //const sourceURL = sourcePath.replace("ski-resorts","resorts");
     const FullURL = HOMEpath + sourcePath;
     // Open the URL
     const response = await page.goto(FullURL, { waitUntil: 'domcontentloaded' });
