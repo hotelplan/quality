cd89cd82315bcfca805cdadc818278ea1d0427ae
xtcar7 <152252927+xtcar7@users.noreply.github.com>
2024-10-29 17:43:41 +0800

    adjusted header test

--- tests/e2e_tests/inw_tests/e_cms_tests/country_page_walking.spec.ts
+++ tests/e2e_tests/inw_tests/e_cms_tests/country_page_walking.spec.ts
@@ -14,10 +14,8 @@ const ECMSurl = environmentBaseUrl[env].e_cms;
 const PCMSurl = environmentBaseUrl[env].p_cms;
 
 // Helper function to read URLs from the CSV file
-const LaplandDatacsv = parse(fs.readFileSync(path.join(__dirname, 'migration_data', 'Migration_Lapland.csv')), {columns: true, skip_empty_lines: true});
-const SantaDatacsv = parse(fs.readFileSync(path.join(__dirname, 'migration_data', 'Migration_SantasBreaks.csv')), {columns: true, skip_empty_lines: true});
-const SkiDatacsv = parse(fs.readFileSync(path.join(__dirname, 'migration_data', 'Migration_Ski.csv')), {columns: true, skip_empty_lines: true});
 const WalkingDatacsv = parse(fs.readFileSync(path.join(__dirname, 'migration_data', 'Migration_Walking.csv')), {columns: true, skip_empty_lines: true});
+const WalkingCountryData = WalkingDatacsv.filter(row => row['Alias'].includes('country'));
 
 //test.beforeAll(async ({page}) => {
     
@@ -36,165 +34,57 @@ test.afterEach(async ({ page },testInfo) => {
 });*/
 
 
-test.describe('Lapland Hero Banner Content Test', () => {
+for(const walkingCountrydata of WalkingCountryData){
 
-  const LaplandCountryData = LaplandDatacsv.filter(row => row['Alias'].includes('country'));
-  const LaplandRegionData = LaplandDatacsv.filter(row => row['Alias'].includes('region'));
-  const LaplandResortData = LaplandDatacsv.filter(row => row['Alias'].includes('resort'));
-  const LaplandAccomodationData = LaplandDatacsv.filter(row => row['Alias'].includes('accommodation'));
-  
+  test.describe(`Walking Country Page (${walkingCountrydata.SourcePath})`, () => {
 
-  for(const laplandCountrydata of LaplandCountryData){
-    test(`Hero Banner Country Page Test (${laplandCountrydata.SourcePath})`, {tag: ['@regression'],}, async ({page, ecmsSignInpage, ecmsMainPage, countryPage}) => {
+    test(`Walking Country Page Header Test (${walkingCountrydata.SourcePath})`, {tag: ['@regression'],}, async ({page, ecmsSignInpage, ecmsMainPage, countryPage}) => {
+      
+      const SiteURL = await ECMS.Walking_URL_Builder(ECMSurl, walkingCountrydata.SourcePath);
+
+      await page.goto(SiteURL, { waitUntil: 'domcontentloaded' });
+      await countryPage.Check_Walking_Country_Page_Header();  
+
+    });
+
+
+    test(`Hero Banner Country Page Test (${walkingCountrydata.SourcePath})`, {tag: ['@regression'],}, async ({page, ecmsSignInpage, ecmsMainPage, countryPage}) => {
       test.slow();
 
-      const target = laplandCountrydata.SourcePath.split('\\').pop()
+      const target = walkingCountrydata.SourcePath.split('\\').pop()
         ?.split('-')
         .map(word => word.charAt(0).toUpperCase() + word.slice(1))
         .join(' ');
       console.log('Target:', target);
-      console.log('Username:', process.env.ECMS_USERNAME);
-      console.log('Password:', process.env.ECMS_PASSWORD);
 
       await page.goto(ECMSurl+'/umbraco/login',{ waitUntil: 'domcontentloaded' });
       await ecmsSignInpage.ECMS_Login(process.env.ECMS_USERNAME,process.env.ECMS_PASSWORD);
-      await ecmsMainPage.ECMS_Expand_Tree("Lapland Holidays", null, target, null, null);
+      await ecmsMainPage.ECMS_Expand_Tree("Walking Holidays", null, target, null, null);
       await ecmsMainPage.ECMS_Select_Target_Page(target);
       await ecmsMainPage.ECMS_Modify_Hero_Banner("291A0817");
 
-      const SiteURL = await ECMS.Lapland_URL_Builder(ECMSurl, laplandCountrydata.SourcePath);
+      const SiteURL = await ECMS.Walking_URL_Builder(ECMSurl, walkingCountrydata.SourcePath);
       // Open the URL
       await page.goto(SiteURL, { waitUntil: 'domcontentloaded' });
       await countryPage.Country_Hero_Banner_Checker("291A0817","Full Bleed","Top","Full");
-      
+        
     });
-  }
-
-});
 
 
 
-test.describe('Ski Hero Banner Content Test', () => {
 
-  const SkiCountryData = SkiDatacsv.filter(row => row['Alias'].includes('country'));
-  const SkiRegionData = SkiDatacsv.filter(row => row['Alias'].includes('region'));
-  const SkiResortData = SkiDatacsv.filter(row => row['Alias'].includes('resort'));
-  const SkiAccomodationData = SkiDatacsv.filter(row => row['Alias'].includes('accommodation'));
-  
-  test.describe('Ski Country Page', () => {
-
-    for(const skiCountrydata of SkiCountryData){
-      test(`Hero Banner Country Page Test (${skiCountrydata.SourcePath})`, {tag: ['@smoke', '@regression'],}, async ({page, ecmsSignInpage, ecmsMainPage, countryPage}) => {
-        test.slow();
-
-        const target = skiCountrydata.SourcePath.split('\\').pop()
-          ?.split('-')
-          .map(word => word.charAt(0).toUpperCase() + word.slice(1))
-          .join(' ');
-        console.log('Target:', target);
-        console.log('Username:', process.env.ECMS_USERNAME);
-        console.log('Password:', process.env.ECMS_PASSWORD);
-
-        await page.goto(ECMSurl+'/umbraco/login',{ waitUntil: 'domcontentloaded' });
-        await ecmsSignInpage.ECMS_Login(process.env.ECMS_USERNAME,process.env.ECMS_PASSWORD);
-        await ecmsMainPage.ECMS_Expand_Tree("Ski Holidays", null, target, null, null);
-        await ecmsMainPage.ECMS_Select_Target_Page(target);
-        await ecmsMainPage.ECMS_Modify_Hero_Banner("291A0817");
-
-        const SiteURL = await ECMS.Ski_URL_Builder(ECMSurl, skiCountrydata.SourcePath);
-        // Open the URL
-        await page.goto(SiteURL, { waitUntil: 'domcontentloaded' });
-        await countryPage.Country_Hero_Banner_Checker("291A0817","Full Bleed","Top","Full");
-        
-      });
-    }
-
+    
   });
 
 
-  test.describe('Ski Region Page', () => {
-    for(const skiRegiondata of SkiRegionData){
-      test(`Hero Banner Region Page Test (${skiRegiondata.SourcePath})`, {tag: ['@regression'],}, async ({page, ecmsSignInpage, ecmsMainPage, regionPage}) => {
-        test.slow();
-
-        console.log('Region:', skiRegiondata.Region);
-        const target = skiRegiondata.Region.split('-')
-        .map(part => part.split(' ')
-            .map(word => {
-                if (word.includes('’')) {
-                    const parts = word.split('’');
-                    return parts[0].charAt(0).toLowerCase() + '’' + parts[1].charAt(0).toUpperCase() + parts[1].slice(1).toLowerCase();
-                } else {
-                    return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
-                }
-            })
-            .join(' ')
-        )
-        .join('-');
-        console.log('Target:', target);
-
-        const country = skiRegiondata.Country.split('\\').pop()
-        ?.split('-')
-        .map(word => word.split(' ')
-            .map(subWord => subWord.charAt(0).toUpperCase() + subWord.slice(1).toLowerCase())
-            .join(' ')
-        )
-        .join('-');
-        console.log('Country:', country);
-
-        await page.goto(ECMSurl+'/umbraco/login',{ waitUntil: 'domcontentloaded' });
-        await ecmsSignInpage.ECMS_Login(process.env.ECMS_USERNAME,process.env.ECMS_PASSWORD);
-        await ecmsMainPage.ECMS_Expand_Tree("Ski Holidays", null, country, target, null);
-        await ecmsMainPage.ECMS_Select_Target_Page(target);
-        await ecmsMainPage.ECMS_Modify_Hero_Banner("291A0817");
-
-        const SiteURL = await ECMS.Ski_URL_Builder(ECMSurl, skiRegiondata.SourcePath);
-        // Open the URL
-        await page.goto(SiteURL, { waitUntil: 'domcontentloaded' });
-        await regionPage.Region_Hero_Banner_Checker("291A0817","Full Bleed","Top","Full");
-        
-      });
-    }
-
-  });
-
-});
 
 
-test.describe('Walking Hero Banner Content Test', () => {
 
-  const WalkingCountryData = WalkingDatacsv.filter(row => row['Alias'].includes('country'));
-  const WalkingRegionData = WalkingDatacsv.filter(row => row['Alias'].includes('region'));
-  const WalkingResortData = WalkingDatacsv.filter(row => row['Alias'].includes('resort'));
-  const WalkingAccomodationData = WalkingDatacsv.filter(row => row['Alias'].includes('accommodation'));
-  
-  test.describe('Walking Country Page', () => {
 
-    for(const walkingCountrydata of WalkingCountryData){
-      test(`Hero Banner Country Page Test (${walkingCountrydata.SourcePath})`, {tag: ['@regression'],}, async ({page, ecmsSignInpage, ecmsMainPage, countryPage}) => {
-        test.slow();
 
-        const target = walkingCountrydata.SourcePath.split('\\').pop()
-          ?.split('-')
-          .map(word => word.charAt(0).toUpperCase() + word.slice(1))
-          .join(' ');
-        console.log('Target:', target);
 
-        await page.goto(ECMSurl+'/umbraco/login',{ waitUntil: 'domcontentloaded' });
-        await ecmsSignInpage.ECMS_Login(process.env.ECMS_USERNAME,process.env.ECMS_PASSWORD);
-        await ecmsMainPage.ECMS_Expand_Tree("Walking Holidays", null, target, null, null);
-        await ecmsMainPage.ECMS_Select_Target_Page(target);
-        await ecmsMainPage.ECMS_Modify_Hero_Banner("291A0817");
 
-        const SiteURL = await ECMS.Walking_URL_Builder(ECMSurl, walkingCountrydata.SourcePath);
-        // Open the URL
-        await page.goto(SiteURL, { waitUntil: 'domcontentloaded' });
-        await countryPage.Country_Hero_Banner_Checker("291A0817","Full Bleed","Top","Full");
-        
-      });
-    }
 
-  });
 
-});
+}
 
--- tests/resources/page_objects/country_page.ts
+++ tests/resources/page_objects/country_page.ts
@@ -3,12 +3,28 @@ import { type Page, type Locator, expect } from '@playwright/test';
 export class CountryPage{
     //variables
     readonly page: Page;
+
+    readonly Country_Header_Home_Icon: Locator;
+    readonly Country_Header_Logo: Locator;
+    readonly Country_Header_Product: Locator;
+    readonly Country_Header_Navigation: Locator;
+    readonly Country_Header_Contact: Locator;
+    readonly Country_Header_User: Locator;
+
     readonly Country_Hero_Banner: Locator;
 
 
     //locators
     constructor(page: Page) {
         this.page = page;
+
+        this.Country_Header_Home_Icon = page.locator('//div[@class="c-home-menu-wrapper"]');
+        this.Country_Header_Logo = page.locator('//a[@class="c-brand__logo"]');
+        this.Country_Header_Product = page.locator('//span[@class="c-brand__product"]');
+        this.Country_Header_Navigation = page.locator('//nav[@data-module="navigation"]');
+        this.Country_Header_Contact = page.locator('//div[@class="c-brand-contact"]');
+        this.Country_Header_User = page.locator('//span[@class="c-user"]');
+
         this.Country_Hero_Banner = page.locator('//div[contains(@class,"c-hero is-active")]');
 
 
@@ -33,11 +49,11 @@ export class CountryPage{
         await expect(Style).toContain(mediaName);
 
         if(vertical){
-            await expect(ClassAttribute).toContain("c-hero__content-alignment-vertical-" + verticalname);
+            await expect(ClassAttribute).toContain("alignment-vertical-" + verticalname);
         }
 
         if(horizontal){
-            await expect(ClassAttribute).toContain("c-hero__content-alignment-" + horizontalname);
+            await expect(ClassAttribute).toContain("alignment-horizontal-" + horizontalname);
         }
 
     }
@@ -45,6 +61,23 @@ export class CountryPage{
 
 
     //assertions
+    async Check_Walking_Country_Page_Header() {
+        await expect(this.Country_Header_Home_Icon).toBeVisible({timeout: 30000});
+        await expect(this.Country_Header_Logo).toBeVisible({timeout: 30000});
+        await expect(this.Country_Header_Product).toBeVisible({timeout: 30000});
+        await expect(this.Country_Header_Navigation).toBeVisible({timeout: 30000});
+        await expect(this.Country_Header_Contact).toBeVisible({timeout: 30000});
+        await expect(this.Country_Header_User).toBeVisible({timeout: 30000});
+
+        await expect(this.Country_Header_Product).toContainText('WALKING');
+
+        await expect(this.Country_Header_Navigation).toContainText('Destinations');
+        await expect(this.Country_Header_Navigation).toContainText('Holiday Types');
+        await expect(this.Country_Header_Navigation).toContainText('Lakes and Mountains');
+        await expect(this.Country_Header_Navigation).toContainText('Holiday by Train');
+        await expect(this.Country_Header_Navigation).toContainText('Inspire Me');
+        await expect(this.Country_Header_Navigation).toContainText('Deals and Offers');
+    }
 
 
 }
