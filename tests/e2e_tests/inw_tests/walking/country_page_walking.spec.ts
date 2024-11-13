7a670af0862a019d2103be192d5a373c5be722bb
xtcar7 <152252927+xtcar7@users.noreply.github.com>
2024-11-13 18:51:48 +0800

    modified workflow

--- tests/e2e_tests/inw_tests/walking/country_page_walking.spec.ts
+++ tests/e2e_tests/inw_tests/walking/country_page_walking.spec.ts
@@ -37,6 +37,7 @@ test.afterEach(async ({ page },testInfo) => {
 for(const walkingCountrydata of WalkingCountryData){
 
   test.describe(`Walking Country Page (${walkingCountrydata.SourcePath})`, () => {
+    test.describe.configure({mode: "serial"});
 
     test(`Header-Footer Test Walking Country Page (${walkingCountrydata.SourcePath})`, {tag: ['@regression'],}, async ({page, ecmsSignInpage, ecmsMainPage, countryPage}) => {
       test.setTimeout(180000);
@@ -119,6 +120,29 @@ for(const walkingCountrydata of WalkingCountryData){
         
     });
     
+
+    test(`Accordions Test Walking Country Page (${walkingCountrydata.SourcePath})`, {tag: ['@regression'],}, async ({page, ecmsSignInpage, ecmsMainPage, countryPage}) => {
+      test.setTimeout(180000);
+
+      const target = walkingCountrydata.SourcePath.split('\\').pop()
+        ?.split('-')
+        .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+        .join(' ');
+      console.log('Target:', target);
+
+      await page.goto(ECMSurl+'/umbraco/login',{ waitUntil: 'domcontentloaded' });
+      await ecmsSignInpage.ECMS_Login(process.env.ECMS_USERNAME,process.env.ECMS_PASSWORD);
+      await ecmsMainPage.ECMS_Expand_Tree("Walking Holidays", null, target, null, null);
+      await ecmsMainPage.ECMS_Select_Target_Page(target);
+      
+      //await ecmsMainPage.ECMS_Modify_Hero_Banner("291A0817");
+
+      const SiteURL = await ECMS.Walking_URL_Builder(ECMSurl, walkingCountrydata.SourcePath);
+      // Open the URL
+      await page.goto(SiteURL, { waitUntil: 'domcontentloaded' });
+      await countryPage.Country_Hero_Banner_Checker("291A0817","Full Bleed","Top","Full");
+        
+    });
 
 
 
