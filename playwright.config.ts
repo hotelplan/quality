3a137d7cbd2d4fed2c56f68a7a1dba2de8172716
Vi Anne Eliseo <vi.eliseo@hotelplan.co.uk>
2025-03-19 18:58:29 +0800

    update playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -21,7 +21,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [['html', { outputFolder: 'test-results' }]],
+  reporter: [["line"], ["allure-playwright"]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     headless: false,
