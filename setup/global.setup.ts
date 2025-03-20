fa6c0819bf6f9d07975384404feb4c9e4af05487
Vi Anne Eliseo <vi.eliseo@hotelplan.co.uk>
2025-03-20 20:00:01 +0800

    added the pcms login in global setup

--- setup/global.setup.ts
+++ setup/global.setup.ts
@@ -3,13 +3,24 @@ import environmentBaseUrl from '../tests/resources/utils/environmentBaseUrl';
 
 const env = process.env.ENV || "qa";
 const ECMSurl = environmentBaseUrl[env].e_cms;
+const PCMSurl = environmentBaseUrl[env].p_cms;
 
 setup(`ECMS User Login`, async ({ page, ecmsSignInpage }) => {
-    console.log('CMS User setting up...')
+    console.log('ECMS User setting up...')
 
     await page.goto(ECMSurl + '/umbraco/login', { waitUntil: 'domcontentloaded' });
     await ecmsSignInpage.ECMS_Login(process.env.ECMS_USERNAME, process.env.ECMS_PASSWORD);
-    await page.context().storageState({ path: '.auth/cmsUserStorageState.json' as string });
-    console.log('CMS User  Successfully Login...')
+    await page.context().storageState({ path: '.auth/ecmsUserStorageState.json' as string });
+    console.log('ECMS User  Successfully Login...')
+
+});
+
+setup(`PCMS User Login`, async ({ page, ecmsSignInpage }) => {
+    console.log('PCMS User setting up...')
+
+    await page.goto(PCMSurl + '/umbraco/login', { waitUntil: 'domcontentloaded' });
+    await ecmsSignInpage.ECMS_Login(process.env.PCMS_USERNAME, process.env.PCMS_PASSWORD);
+    await page.context().storageState({ path: '.auth/pcmsUserStorageState.json' as string });
+    console.log('PCMS User  Successfully Login...')
 
 });
