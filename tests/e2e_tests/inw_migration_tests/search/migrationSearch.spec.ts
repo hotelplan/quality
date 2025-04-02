65999515ec08f49060e56cd496b2336183fcc04e
xtcar7 <152252927+xtcar7@users.noreply.github.com>
2025-04-02 23:39:42 +0800

    migration test init

---
+++ tests/e2e_tests/inw_migration_tests/search/migrationSearch.spec.ts
@@ -0,0 +1,44 @@
+import { test } from '../../../resources/inw_resources/page_objects/base/page_base';
+import environmentBaseUrl from '../../../resources/utils/environmentBaseUrl';
+
+const env = process.env.ENV || "qa";
+const ECMSurl = environmentBaseUrl[env].e_cms;
+
+const products = ['Ski', 'Walking', 'Lapland'];
+
+test.beforeEach(async ({ page }) => {
+    await test.step('Given: I navigate to home page', async () => {
+        await page.goto(ECMSurl + '/');
+    });
+});
+
+test.describe('Broad Search', async () => {
+    for (const product of products) {
+        test(`The broad search proceeds with default values for ${product} holidays @inw`, async ({ searchResultPage}) => {
+            test.setTimeout(120000);
+            await test.step(`Given: I sleect a product to search`, async () => {
+                await searchResultPage.clickSearchProductTab(product);
+            });
+
+            //search criteria
+            
+            await test.step(`And: I search for holidays`, async () => {
+                await searchResultPage.clickSearchHolidayBtn();
+            });
+
+            await test.step('And: I navigate to Search results page', async () => {
+                await searchResultPage.validateSearchResultPageUrl();
+            });
+
+            await test.step('And: I check the Search results criteria', async () => {
+                await searchResultPage.checkCriteriaBarContent('Any date (7 nights)');
+                await searchResultPage.checkCriteriaBarContent('2 adults');
+                await searchResultPage.checkCriteriaBarContent('From Any departure location');
+            });
+
+            await test.step('When : I check Accomodation cards search results', async () => {
+                await searchResultPage.countAccommodationCards();
+            });
+        });
+    }
+});
