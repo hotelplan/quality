607e77137f7d1a4ba31d888b2b6b2d5dca9eef0a
Vi Anne Eliseo <vi.eliseo@hotelplan.co.uk>
2025-04-30 19:54:44 +0800

    cta button spec file

---
+++ tests/e2e_tests/inw_tests/components/ctaButton.spec.ts
@@ -0,0 +1,63 @@
+import { test } from '../../../resources/inw_resources/page_objects/base/page_base';
+import environmentBaseUrl from '../../../resources/utils/environmentBaseUrl';
+
+const env = process.env.ENV || "qa";
+const ECMSurl = environmentBaseUrl[env].e_cms;
+
+test.beforeEach(async ({ page }) => {
+    await test.step('Given: I navigate to home page', async () => {
+        await page.goto(ECMSurl + '/umbraco#/content')
+    })
+})
+
+test.describe('Accordion', async () => {
+    test.use({ storageState: '.auth/ecmsUserStorageState.json' });
+    test(`An ECMS user creates a CTA button component and views it on the General Content page @inw`, async ({ ctaButtonComponent, sharedSteps }) => {
+        await test.step(`Given: I select a Generic Content page`, async () => {
+            await sharedSteps.searchAndSelectGenericContentPage()
+        });
+
+        await test.step(`And: I click 'Content' tab`, async () => {
+            await sharedSteps.clickContentTab()
+        });
+
+        await test.step(`And: I click 'Add content' tab`, async () => {
+            await sharedSteps.clickAddContentBtn()
+
+        });
+
+        await test.step(`And: I search for the CTA button component`, async () => {
+            await sharedSteps.searchComponent('CTA button')
+
+        });
+
+        await test.step(`And: I click the CTA button component`, async () => {
+            await sharedSteps.selectComponent()
+
+        });
+
+        await test.step(`And: I setup a CTA button component`, async () => {
+            await ctaButtonComponent.setupCtaButton()
+
+        });
+
+        // await test.step(`And: I click 'Create' button for CTA button component
+        //                  And: I click 'Save and publish' button`, async () => {
+        //     await sharedSteps.clickCreateBtn()
+        //     await sharedSteps.clickSaveAndPublishBtn()
+        // });
+
+        // await test.step(`When: I click 'Info' tab
+        //                  And: click the link to the page`, async () => {
+        //     await sharedSteps.clickInfoTab()
+        //     newPage = await sharedSteps.clickPageLink()
+        // });
+
+        // await test.step(`And: I redirect the Generic Content page
+        //                  Then: I should see the CTA button displayed on the Generic Content Page with details`, async () => {
+        //     await sharedSteps.validatePageUrl(newPage)
+        // });
+
+    })
+
+})
