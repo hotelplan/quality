376640102dbf42e9ef14fb6ad8584856e8a2248a
Vi Anne Eliseo <vi.eliseo@hotelplan.co.uk>
2025-05-12 17:44:25 +0800

    fixed RTE valdation

--- tests/resources/inw_resources/page_objects/component/richTextEditor.ts
+++ tests/resources/inw_resources/page_objects/component/richTextEditor.ts
@@ -4,25 +4,24 @@ export class RTEComponent {
     readonly page: Page
     readonly rteFrame: FrameLocator
     readonly rteParagraph: Locator
-    readonly mainSiteContent: (context : any) => Locator
+    readonly mainSiteContent: (context: any) => Locator
 
     public randomText: string = faker.lorem.sentence()
-    
+
 
     constructor(page: Page) {
         this.page = page;
         this.rteFrame = page.frameLocator('[title="Rich Text Area"]');
         this.rteParagraph = this.rteFrame.locator('[id ="tinymce"]');
-        this.mainSiteContent = (context : any) => context.locator('//main[@class="site-content"]');
+        this.mainSiteContent = (context: any) => context.locator('//main[@class="site-content"]');
     }
     async setupRTE() {
-        console.log("Random:", this.randomText)
-        await expect(this.rteParagraph).toBeVisible({timeout: 10000})
+        await expect(this.rteParagraph).toBeVisible({ timeout: 10000 })
         await this.rteParagraph.fill(this.randomText);
     }
 
     async validateRTE(newPage) {
-        await expect(this.mainSiteContent(newPage), "RTE text is available on the page").toContainText(this.randomText, {timeout: 30000});
+        await expect(newPage.locator('body'), "RTE text is available on the page").toContainText(this.randomText);
     }
 
 }
