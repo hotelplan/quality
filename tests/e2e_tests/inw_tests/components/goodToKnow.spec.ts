3a5070d1a86db159fee1ac7f7daeda44c6fcce8d
Vi Anne Eliseo <vi.eliseo@hotelplan.co.uk>
2025-05-15 20:53:31 +0800

    good-to-know component creation

---
+++ tests/e2e_tests/inw_tests/components/goodToKnow.spec.ts
@@ -0,0 +1,96 @@
+import { test } from '../../../resources/inw_resources/page_objects/base/page_base';
+import environmentBaseUrl from '../../../resources/utils/environmentBaseUrl';
+
+const env = process.env.ENV || "qa";
+const ECMSurl = environmentBaseUrl[env].e_cms;
+let newPage
+
+test.beforeEach(async ({ page }) => {
+    await test.step('Given: I navigate to home page', async () => {
+        await page.goto('https://localhost:7040' + '/umbraco#/content')
+    })
+})
+
+test.describe('Good to know', async () => {
+    test.use({ storageState: '.auth/ecmsUserStorageState.json' });
+    test.only(`An ECMS user creates Good-to-know component and views it on the General Content page @inw`, async ({ goodToKnowComponent, sharedSteps }) => {
+        await test.step(`Given: I select a Generic Content page`, async () => {
+            await sharedSteps.searchAndSelectGenericContentPage()
+        });
+
+        await test.step(`And: I click 'Content' tab`, async () => {
+            await sharedSteps.clickContentTab()
+        });
+
+        await test.step(`And: I click 'Add content' tab`, async () => {
+            await sharedSteps.clickAddContentBtn()
+
+        });
+
+        await test.step(`And: I search for the Good-to-know component`, async () => {
+            await sharedSteps.searchComponent('Good to know')
+
+        });
+
+        await test.step(`And: I click the Good-to-know component`, async () => {
+            await sharedSteps.selectComponent()
+
+        });
+
+        await test.step(`And: I fill out Good-to-know title`, async () => {
+            await goodToKnowComponent.fillOutGoodToKnowTitle()
+        });
+
+        await test.step(`And: I fill out Good-to-know Description`, async () => {
+            await goodToKnowComponent.fillOutGoodToKnowDescription()
+        });
+
+        // Creates 6 Good-to-know items
+        for (let i = 0; i <= 5; i++) {
+            await test.step(`And: I click "Add Good to know item" button`, async () => {
+                await goodToKnowComponent.clickGoodToKnowItemBtn()
+            });
+
+            await test.step(`And: I select a Good-to-know item icon`, async () => {
+                await goodToKnowComponent.selectGoodToKnowItemIcon()
+            });
+
+            await test.step(`And: I fill out Good-to-know item title`, async () => {
+                await goodToKnowComponent.fillOutGoodToKnowItemTitle()
+            });
+
+            await test.step(`And: I fill out Good-to-know item description`, async () => {
+                await goodToKnowComponent.fillOutGoodToKnowItemDescription()
+            });
+
+            await test.step(`And: I fill out Good-to-know item link`, async () => {
+                await goodToKnowComponent.fillOutGoodToKnowItemLink()
+            });
+
+            await test.step(`And: I click 'Create' button for Good-to-know item`, async () => {
+                await goodToKnowComponent.clickSubmitGoodToKnowItem()
+            });
+        }
+
+        await test.step(`And: I click 'Create' button for Good-to-know component
+                         And: I click 'Save and publish' button`, async () => {
+            await sharedSteps.clickCreateBtn(1)
+            await sharedSteps.clickSaveAndPublishBtn()
+        });
+
+        await test.step(`When: I click 'Info' tab
+                         And: click the link to the page`, async () => {
+            await sharedSteps.clickInfoTab()
+            newPage = await sharedSteps.clickPageLink()
+        });
+
+        await test.step(`And: I redirect the Generic Content page
+                         Then: I should see the Pills displayed on the Generic Content Page with details`, async () => {
+            await sharedSteps.validatePageUrl(newPage)
+            await goodToKnowComponent.validateGoodToKnowAvailability(newPage)
+
+        });
+
+    })
+
+})
